6bd933bdc1b2a8721427d8295b0c03a7c8f14b06
shun <shun@getto.systems>
2018-06-20 22:53:17 +0900

    fix: indent

--- src/Getto.elm
+++ src/Getto.elm
@@ -17,33 +17,33 @@ type alias Flags =
   { storage :
     { global :
       { credential : Encode.Value
-      , menu : Encode.Value
-      , terminal : Encode.Value
+      , menu       : Encode.Value
+      , terminal   : Encode.Value
       }
     , page : List ( String, Encode.Value )
     }
-  , page : Page
+  , page    : Page
   , project : Project
   }
 
 type alias Opts =
-  { version : String
+  { version      : String
   , authRequired : Bool
   }
 
 type alias GeneralInfo info =
   { info
   | application : Application
-  , api : Api
-  , page : Page
-  , project : Project
-  , credential : Credential
+  , api         : Api
+  , page        : Page
+  , project     : Project
+  , credential  : Credential
   }
 
 type alias Info = GeneralInfo {}
 
 type alias Application =
-  { version : String
+  { version   : String
   , copyright : String
   }
 
@@ -52,27 +52,27 @@ type alias Api =
   }
 
 type alias Page =
-  { name : String
-  , path : String
+  { name   : String
+  , path   : String
   , search : Location.Search
-  , query : String
+  , query  : String
   , loadAt : String
   }
 
 type alias Project =
-  { name : String
-  , company : String
-  , title : String
+  { name     : String
+  , company  : String
+  , title    : String
   , subTitle : String
   }
 
 type alias Credential =
-  { loginID : String
-  , rememberMe : Bool
-  , role : List String
-  , token : Maybe String
-  , oldToken : Maybe String
-  , previous : Maybe String
-  , renewedAt : Maybe String
+  { loginID      : String
+  , rememberMe   : Bool
+  , role         : List String
+  , token        : Maybe String
+  , oldToken     : Maybe String
+  , previous     : Maybe String
+  , renewedAt    : Maybe String
   , authRequired : Bool
   }
